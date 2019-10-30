c58665f6d68275e2ba78f5b6bcc7c8be7c9d99f1
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-30 15:10:25 -0700

    wrote validateUserId and validateUser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const server = express();
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -14,3 +15,4 @@ function logger(req, res, next) {
 };
 
 module.exports = server;
+
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,8 @@ const express = 'express';
 
 const router = express.Router();
 
+const db = require("./userDb");
+
 router.post('/', (req, res) => {
 
 });
@@ -32,16 +34,34 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
+async function validateUserId(req, res, next) {
+    const { id } = req.params;
+    try {
+        const user = await db.getById(id);
+        if (!user) {
+            res.status(400).json({ message: "invalid user id" });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(500).json({ errorMessage: "Internal server error", error })
+    }
 };
 
 function validateUser(req, res, next) {
-
+    const body = req.body;
+    if (!body) {
+        res.status(400).json({ message: "missing user data" });
+    }
+    if (!body.name) {
+        res.status(400).json({ message: "missing required name field" });
+    }
+    next();
 };
 
 function validatePost(req, res, next) {
-
+    const body = req.body;
+    
 };
 
 module.exports = router;
